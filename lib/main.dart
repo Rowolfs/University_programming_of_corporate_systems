a5c7c750313081b2701f70d74f0fdcc00c5cd66d
Rowolfs <ro.volkov2018@yandex.ru>
2025-09-30 00:05:38 +0300

    end 4 practic

--- lib/main.dart
+++ lib/main.dart
@@ -2,38 +2,94 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  int counter = 0;
+
+  void increment() {
+    setState(() {
+      counter++;
+    });
+  }
+
+  void decrement() {
+    setState(() {
+      counter--;
+      if (counter<0) setZero();
+    });
+  }
+
+  void onLongIncrement(){
+    setState(() {
+      counter += 10;
+    });
+  }
+  void onLongDicrement(){
+    setState(() {
+      counter -= 10;
+      if (counter<0) setZero();
+    });
+  }
+  void setZero(){
+    setState(() {
+      counter = 0;
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       home: Scaffold(
-        appBar: AppBar(title: Text("(Волков Роман ЭФБО-09-23) привет это моя 3 практика 👽"), centerTitle: true,),
-        body: Center(child: Column( 
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text("Что то тут надо написать"),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text("Попробуй нажать "),
-            ),
-            Row(mainAxisAlignment: MainAxisAlignment.center,
+        appBar: AppBar(
+          title: Text("(Волков Роман ЭФБО-09-23) привет это моя 4 практика 👽"),
+          centerTitle: true,
+        ),
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Text("Проверка   ", style: GoogleFonts.montserratAlternates(color: Colors.amber, fontSize: 35)),
-              Text("      🥶", style: TextStyle(fontSize: 67),)
-            ],),
-            Container(width: 50, height: 50, decoration:  BoxDecoration(color: Color(0xFFefe4b0), borderRadius: BorderRadius.circular(10)),)
-          ],
+              Text(
+                "Счетчик: $counter",
+                style: GoogleFonts.anekMalayalam(fontSize: 94),
+              ),
+              SizedBox(height: 20),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                   Container(decoration: BoxDecoration(color: Colors.blue, borderRadius: BorderRadius.circular(10)), padding: EdgeInsets.all(16), child:
+                  ElevatedButton(
+                    onPressed: increment,
+                    onLongPress: onLongIncrement,
+                    child: Text("Увеличить"),
+                  )),
+
+                  Container(decoration: BoxDecoration(color: Colors.teal, borderRadius: BorderRadius.circular(10)), padding: EdgeInsets.all(16), child:
+                  ElevatedButton(
+                    onPressed: decrement,
+                    onLongPress: onLongDicrement,
+                    child: Text("Уменьшить"),
+                  ))
+
+                ],),
+              Container(decoration: BoxDecoration(color: Colors.deepOrange, borderRadius: BorderRadius.circular(10)), padding: EdgeInsets.all(16), child:
+                  ElevatedButton(
+                    onPressed: setZero,
+                    child: Text("Сбросить"),
+                  ))
+              
+            ],
+          ),
         ),
       ),
-      )
-
     );
   }
 }
